4d57e692c2bc143a064594a3873cf5a2f25fbec4
FacturaOne <65960142+FacturaOne@users.noreply.github.com>
2024-06-08 19:01:06 +0200

    actualizacion

--- concatena-hash.php
+++ concatena-hash.php
@@ -1,28 +1,9 @@
 <?php 
-// Información de la factura
-$nif = "00000006Y";
-$numSerieFactura = "1.1.1.8";
-$fechaExpedicionFactura = "09-04-2024";
-$tipoFactura = "F2";
-$importeTotal = "2.01";
-$cuotaTotal = "0.00";
-$huellaAnterior = "E6FE58EE455F233BFA429FA7A9D90EDC006CBB2421876EB2590D37E682414CB2";
-$fechaHoraHusoGenRegistro = "2024-04-09T19:12:03+01";
+// https://www.agenciatributaria.es/static_files/AEAT_Desarrolladores/EEDD/IVA/VERI-FACTU/Veri-Factu_especificaciones_huella_hash_registros.pdf
 
-// Concatenar los valores en el orden especificado
-$cadenaVerifactu = $nif .
-                   $numSerieFactura .
-                   $fechaExpedicionFactura .
-                   $tipoFactura .
-                   $importeTotal .
-                   $cuotaTotal .
-                   $huellaAnterior .
-                   $fechaHoraHusoGenRegistro;
-
-// Calcular el hash SHA-256 de la cadena concatenada
-$hashVerifactu = hash('sha256', $cadenaVerifactu);
-
-// Mostrar el resultado
-echo "Cadena concatenada: " . $cadenaVerifactu . "\n<br><br>";
-echo "Hash SHA-256: " . $hashVerifactu . "\n<br><br>";
+function getHashVerifactu($idEmisoraFactura, $numFacturaSerie, $fechaExpedicion, $tipoFactura, $cuotaTotal, $importeTotal, $huellaAnterior, $fechaUsoHorarioRegistro){ 
+	$cadena = "IDEmisorFactura=$idEmisoraFactura&NumSerieFactura=$numFacturaSerie&FechaExpedicionFactura=$fechaExpedicion&TipoFactura=$tipoFactura&CuotaTotal=$cuotaTotal&ImporteTotal=$importeTotal&Huella=$huellaAnterior&FechaHoraHusoGenRegistro=$fechaUsoHorarioRegistro";
+	
+	return hash('sha256', $cadena); 
+}
 ?>
